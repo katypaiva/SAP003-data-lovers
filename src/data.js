a1adf3309c244dba16bbedcf3c02a5e50ac25361
katypaiva <katyfeitozaa@gmail.com>
2019-09-18 14:15:15 -0300

    atualizando o css, html e main.js

--- src/data.js
+++ src/data.js
@@ -1,5 +1,3 @@
-// esta é uma função de exemplo
-// veja como agregamos a função ao objeto global window
 function filterData(data, condition){
   return data.filter(item => item.type.includes(condition));
 }
