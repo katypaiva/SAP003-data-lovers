e7e1b54d34dde89105f09ccc90ec6a7ce518c21b
katypaiva <katyfeitozaa@gmail.com>
2019-09-26 14:38:23 -0300

    testes e funções de filtro

--- src/main.js
+++ src/main.js
@@ -2,49 +2,69 @@ const pokemonData = POKEMON.pokemon;
 const main = document.getElementById("content");
 const btnType = document.getElementById("btn-type");
 const btnOrder = document.getElementById("btn-order");
-
-btnOrder.addEventListener ("click", () => {
-  main.innerHTML = "";
-  template(sortData(pokemonData));   
-});
-
-btnType.addEventListener("change", () => {
-  main.innerHTML = "";
-  template(app.filterData(pokemonData, btnType.value))
-});
-
-template(pokemonData);
-
-window.onload = () => {
-  menuTypes(pokemonData);
-}
-
-function template(arr) {
- arr.forEach(poke => {
-    main.innerHTML += `
-<div class ="card card${poke.type[0]}"> 
-<img class="image" src="${poke.img}"/>
-<p class="nameOf"> ${poke.name}<p>
-<div class="info">
-<p><strong>Pokedex:</strong> ${poke.id}</p>
-<p><strong>Type:</strong> ${poke.type}</p>
-<p><strong>weaknesses:</strong> ${poke.weaknesses}</p>
-</div>
-</div>`})
-}
-
+const calc = document.getElementById("result");
+const searchText = document.getElementById("searchtxt");
+const searchBtn = document.getElementById("btn-search");
 
 function menuTypes(data) {
   let arrTypes = [];
+  let opt = "";
   data.map(poke => poke.type.map(type => {
     if (!arrTypes.includes(type)) {
       arrTypes.push(type);
+      opt += `<option class="${type}" value="${type}">${type}</option>`;
     } else {
       return false;
     }
 
-  }))
-  btnType.innerHTML = `<option value="none" >Selecione o filtro</option>`;
-  btnType.innerHTML += arrTypes.map(type => `<option class="${type}" value="${type}">${type}</option>`).join("");
+  }));
 
+  btnType.innerHTML = "<option value=\"none\" >Filter</option>";
+  btnType.innerHTML += opt;
 }
+
+searchText.addEventListener("keydown", (event) => {
+  if (event.keyCode !== 13) {
+    main.innerHTML = "";
+    return template(window.app.filterName(pokemonData, searchText.value));
+  }
+})
+
+searchBtn.addEventListener("click", () => {
+  main.innerHTML = "";
+  template(window.app.filterName(pokemonData, searchText.value));
+});
+
+btnOrder.addEventListener("change", () => {
+  main.innerHTML = "";
+  template(window.app.sortData(pokemonData, "name", btnOrder.value));
+});
+
+btnType.addEventListener("change", () => {
+  main.innerHTML = "";
+  template(window.app.filterData(pokemonData, btnType.value)),
+  calc.innerHTML = `Existem ${window.app.computers(window.app.filterData(pokemonData, btnType.value))} pokemons do tipo ${btnType.value}`;
+});
+
+window.onload = () => {
+  menuTypes(pokemonData); 
+  template(pokemonData);  
+};
+
+function template(arr) {
+  let template = "";
+  arr.forEach(poke => {
+    template += `
+<div class ="card"> 
+<img class="image" src="${poke.img}"/>
+<p class="nameOf"> ${poke.name}<p>
+<div class="info card${poke.type[0]}">
+<p><strong>Pokedex:</strong> ${poke.id}</p>
+<p><strong>Type:</strong> ${poke.type.join(", ")}</p>
+<p><strong>Weaknesses:</strong> ${poke.weaknesses.join(", ")}</p>
+</div>
+</div>`;});
+
+  main.innerHTML += template;
+}
+
