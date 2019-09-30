3160f2ef4027728c41d7e082e991ff3c27f39390
Joanilza Bockhorny <jbockhorny@gmail.com>
2019-09-29 21:29:51 -0300

    readme

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,48 +1,12 @@
 require("../src/data.js");
 
-const pokemonData = [{
-  "id": 1,
-  "num": "001",
-  "name": "Bulbasaur",
-  "type": [
-    "Grass",
-    "Poison"
-  ],
-    
-}, {
-  "id": 2,
-  "num": "002",
-  "name": "Ivysaur",
-  "type": [
-    "Grass",
-    "Poison"
-  ],
-    
-}, {
-  "id": 3,
-  "num": "003",
-  "name": "Venusaur",
-  "type": [
-    "Grass",
-    "Poison"
-  ],
-
-}, {
-  "id": 4,
-  "num": "004",
-  "name": "Charmander",
-  "type": [
-    "Fire"
-  ],
-    
-}, {
-  "id": 5,
-  "num": "005",
-  "name": "Charmeleon",
-  "type": [
-    "Fire"
-  ],
-}];
+const pokemonData = [
+  { "id": 1, "num": "001", "name": "Bulbasaur", "type": ["Grass", "Poison"] },
+  { "id": 2, "num": "002", "name": "Ivysaur", "type": ["Grass", "Poison"] },
+  { "id": 3, "num": "003", "name": "Venusaur", "type": ["Grass", "Poison"] },
+  { "id": 4, "num": "004", "name": "Charmander", "type": ["Fire"] },
+  { "id": 5, "num": "005", "name": "Charmeleon", "type": ["Fire"] }
+];
 
 describe("filterData", () => {
   it("is a function", () => {
@@ -50,23 +14,30 @@ describe("filterData", () => {
   });
 
   it("returns `example`", () => {
-         
-    expect(app.filterData(pokemonData, "Fire")).toEqual([{
-      "id": 4,
-      "num": "004",
-      "name": "Charmander",
-      "type": [
-        "Fire"
-      ],
-      
-    }, {
-      "id": 5,
-      "num": "005",
-      "name": "Charmeleon",
-      "type": [
-        "Fire"
-      ],
-    }]);
+
+    expect(app.filterData(pokemonData, "Fire")).toEqual
+    ([
+      { "id": 4, "num": "004", "name": "Charmander", "type": ["Fire"] },
+      { "id": 5, "num": "005", "name": "Charmeleon", "type": ["Fire"] }
+    ]);
+  });
+});
+
+describe("sortData", () => {
+  it("is a function", () => {
+    expect(typeof app.sortData).toEqual("function");
+  });
+
+  it("returns `example`", () => {
+
+    expect(app.sortData(pokemonData, "name", "ascendingOrder")).toEqual
+    ([
+      { "id": 1, "num": "001", "name": "Bulbasaur", "type": ["Grass", "Poison"] },
+      { "id": 4, "num": "004", "name": "Charmander", "type": ["Fire"] },
+      { "id": 5, "num": "005", "name": "Charmeleon", "type": ["Fire"] },
+      { "id": 2, "num": "002", "name": "Ivysaur", "type": ["Grass", "Poison"] },
+      { "id": 3, "num": "003", "name": "Venusaur", "type": ["Grass", "Poison"] }
+    ]);
 
   });
 });
@@ -77,98 +48,15 @@ describe("sortData", () => {
   });
 
   it("returns `example`", () => {
-         
-    expect(app.sortData(pokemonData, "name", "ascendingOrder")).toEqual([{
-      "id": 1,
-      "num": "001",
-      "name": "Bulbasaur",
-      "type": [
-        "Grass",
-        "Poison"
-      ],
-    }, {
-      "id": 4,
-      "num": "004",
-      "name": "Charmander",
-      "type": [
-        "Fire"
-      ],
-    }, {
-      "id": 5,
-      "num": "005",
-      "name": "Charmeleon",
-      "type": [
-        "Fire"
-      ],
-    }, {
-      "id": 2,
-      "num": "002",
-      "name": "Ivysaur",
-      "type": [
-        "Grass",
-        "Poison"
-      ],
-        
-    }, {
-      "id": 3,
-      "num": "003",
-      "name": "Venusaur",
-      "type": [
-        "Grass",
-        "Poison"
-      ],
 
-    }]);
-
-  });
-});
-
-describe("sortData", () => {
-  it("is a function", () => {
-    expect(typeof app.sortData).toEqual("function");
-  });
-
-  it("returns `example`", () => {
-         
-    expect(app.sortData(pokemonData, "name", "descendingOrder")).toEqual([{
-      "id": 3,
-      "num": "003",
-      "name": "Venusaur",
-      "type": [
-        "Grass",
-        "Poison"
-      ],
-    }, {
-      "id": 2,
-      "num": "002",
-      "name": "Ivysaur",
-      "type": [
-        "Grass",
-        "Poison"
-      ],
-    }, {
-      "id": 5,
-      "num": "005",
-      "name": "Charmeleon",
-      "type": [
-        "Fire"
-      ],
-    }, {
-      "id": 4,
-      "num": "004",
-      "name": "Charmander",
-      "type": [
-        "Fire"
-      ],
-    }, { "id": 1,
-      "num": "001",
-      "name": "Bulbasaur",
-      "type": [
-        "Grass",
-        "Poison"
-      ],
-    }]);
-
+    expect(app.sortData(pokemonData, "name", "descendingOrder")).toEqual
+    ([
+      { "id": 3, "num": "003", "name": "Venusaur", "type": ["Grass", "Poison"] },
+      { "id": 2, "num": "002", "name": "Ivysaur", "type": ["Grass", "Poison"] },
+      { "id": 5, "num": "005", "name": "Charmeleon", "type": ["Fire"] },
+      { "id": 4, "num": "004", "name": "Charmander", "type": ["Fire"] },
+      { "id": 1, "num": "001", "name": "Bulbasaur", "type": ["Grass", "Poison"] }
+    ]);
   });
 });
 
@@ -178,7 +66,7 @@ describe("computers", () => {
   });
 
   it("returns `1.3`", () => {
-         
+
     expect(app.computers(app.filterData(pokemonData, "Fire"))).toBe("1.3");
   });
 });
@@ -189,14 +77,10 @@ describe("filterName", () => {
   });
 
   it("returns `pokemons`", () => {
-         
-    expect(app.filterName(pokemonData, "Charmander")).toEqual([{
-      "id": 4,
-      "num": "004",
-      "name": "Charmander",
-      "type": [
-        "Fire"
-      ],
-    }]);
+
+    expect(app.filterName(pokemonData, "Charmander")).toEqual
+    ([
+      { "id": 4, "num": "004", "name": "Charmander", "type": ["Fire"] }
+    ]);
   });
 });
